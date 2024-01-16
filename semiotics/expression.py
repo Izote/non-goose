67d9f8206b946f26ae6d56f856c346b0b66d0693
Izote <munguiamv@gmail.com>
2024-01-15 19:10:23 -0500

    rework to include Language class

--- semiotics/expression.py
+++ semiotics/expression.py
@@ -9,23 +9,26 @@ class Expression:
     """
     Represents any Expression (i.e., the observable elements of a Sign)
     """
-    def __init__(self, unit: list[EmicUnit] = None) -> None:
+    def __init__(self, unit: list[EmicUnit] = None,
+                 string: str = None) -> None:
         """
         Constructs an instance of the Expression class and its attributes.
 
         :param unit: a list of EmicUnit instances, where element order
             corresponds to their real-world sequence. Defaults to None.
+
+        :param string: an optional string for use as the `string` attribute.
         """
         self.unit = unit
+
         if self.unit is None:
-            self.str = None
+            self.string = None
         else:
-            self.str = "".join([u.symbol for u in self.unit])
+            if string is None:
+                self.string = "".join([u.symbol for u in self.unit])
+            else:
+                self.string = string
 
     def __repr__(self) -> str:
-        str_unit = f"'{self.str}'" if self.unit else None
-
-        return f"Expression(str={str_unit})"
-
-    def __getitem__(self, item: str):
-        return getattr(self, item)
+        str_unit = f"'{self.string}'" if self.unit else None
+        return f"Expression(string={str_unit})"
