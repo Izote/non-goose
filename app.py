b4040a027a972b0b19d9037c2fc9c3e9eef688fa
mijomu <michael.munguia78@spsmail.cuny.edu>
2024-03-24 12:25:23 -0400

    basic setup

--- app.py
+++ app.py
@@ -1,28 +1,63 @@
 import streamlit as st
+from st_supabase_connection import SupabaseConnection
 
-header = st.header("Submission Form")
 
+@st.cache_resource(ttl=30)
+def load_table():
+    col_order = ["gloss", "autonomous", "mobile", "corporeal", "countable",
+                 "subtle", "cognitive", "visible", "other_perception",
+                 "transforms"]
+    row_data = client.table("properties").select("*").execute().data
+
+    return st.dataframe(row_data, column_order=col_order)
+
+
+client = st.connection("conlang", type=SupabaseConnection)
+
+display_header = st.header("Existing Properties")
+st.write("This section displays properties currently stored in the database.")
+df = load_table()
+
+form_header = st.header("Submit a New Property")
 st.write("Enter a desired gloss and check the box for all true statements.")
-
 gloss = st.text_input("Enter the proposed gloss here.", value="gloss")
-auto = st.checkbox(f"{gloss.upper()} thinks on its own.")
-mobile = st.checkbox(f"{gloss.upper()} can move through space without outside intervention.")
-corp = st.checkbox(f"{gloss.upper()} has a concrete, physical body/form.")
-count = st.checkbox(f"{gloss.upper()} is a discretely countable.")
-cog = st.checkbox(f"{gloss.upper()} is a mental, psychedelic or otherwise cognitive phenomenon.")
 
-per = st.checkbox(f"{gloss.upper()} is primarily perceived without sight.")
+g = gloss.upper()
+text = [f"{g} thinks on its own.",
+        f"{g} can move through space of its own accord.",
+        f"{g} has a concrete, physical body/form.",
+        f"{g} can be counted as one of several discrete entities.",
+        f"{g} can be described as delicate, light, small, soft, subtle, etc.",
+        f"{g} exists only as a cognitive phenomenon.",
+        f"{g} is primarily perceived without sight.",
+        f"{g} can transform, radically changing its state."]
+
+auto = st.checkbox(text[0])
+mobile = st.checkbox(text[1])
+corp = st.checkbox(text[2])
+count = st.checkbox(text[3])
+sub = st.checkbox(text[4])
+cog = st.checkbox(text[5])
+per = st.checkbox(text[6])
+tran = st.checkbox(text[7])
 
 submit = st.button("Submit")
 
 if submit:
-    data = {"gloss": gloss.lower(),
-            "autonomous": int(auto),
-            "mobile": int(mobile),
-            "corporeal": int(corp),
-            "countable": int(count),
-            "cognitive": int(cog),
-            "visible": int(not per),
-            "other_perception": int(per)}
+    new_data = {"gloss": gloss.lower(),
+                "autonomous": int(auto),
+                "mobile": int(mobile),
+                "corporeal": int(corp),
+                "countable": int(count),
+                "subtle": int(sub),
+                "cognitive": int(cog),
+                "visible": int(not per),
+                "other_perception": int(per),
+                "transforms": int(tran)}
 
-    st.write(data)
+    if gloss.lower() != "gloss":
+        try:
+            client.table("properties").insert(new_data, count="None").execute()
+            st.write(f"Properties for `{gloss}` added to database!")
+        except Exception as e:
+            st.write(e)
